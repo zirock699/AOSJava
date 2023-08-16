926a8fa3c2be82fa282bd9092ea5f9768b194274
zirock <zirock699@gmail.com>
2023-08-16 09:14:33 +0000

    Initial commit

---
+++ src/main/java/AOS/UFTDeveloperTest.java
@@ -0,0 +1,45 @@
+package AOS;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import com.hp.lft.sdk.*;
+import com.hp.lft.verifications.*;
+import com.hp.lft.sdk.web.*;
+
+import unittesting.*;
+
+public class UFTDeveloperTest extends UnitTestClassBase {
+
+    public UFTDeveloperTest() {
+        //Change this constructor to private if you supply your own public constructor
+    }
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+        instance = new UFTDeveloperTest();
+        globalSetup(UFTDeveloperTest.class);
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+        globalTearDown();
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void test() throws GeneralLeanFtException {
+        Browser browser = BrowserFactory.launch(BrowserType.CHROME);
+        browser.navigate("https://www.google.com");
+    }
+
+}
